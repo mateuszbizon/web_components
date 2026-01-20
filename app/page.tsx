68a1b7268e558a2bd3db88e6c4e3ec27ddf0627e
Mateusz Bizoń <mat-biz@wp.pl>
2026-01-20 15:09:04 +0100

    added container

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { CodeCard } from "@/components/ui/code-card";
+import Container from "@/components/ui/container";
 import { CONTAINER_TEXT_CODE } from "@/constants/textCodes";
 
 export default function Home() {
   return (
-    <CodeCard
-        code={CONTAINER_TEXT_CODE}
-        title="container.tsx"
-    />
+    <Container>
+        <CodeCard
+            code={CONTAINER_TEXT_CODE}
+            title="container.tsx"
+        />
+    </Container>
   );
 }
---
+++ components/ui/container.tsx
@@ -0,0 +1,14 @@
+import { cn } from '@/lib/utils'
+import React, { ComponentProps } from 'react'
+
+type ContainerProps = ComponentProps<"div">
+
+function Container({ children, className, ...props }: ContainerProps) {
+    return (
+        <div className={cn('size-full max-w-300 mx-auto px-5', className)} {...props}>
+            {children}
+        </div>
+    )
+}
+
+export default Container
